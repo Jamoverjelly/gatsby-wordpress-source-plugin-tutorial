7d36748dfab8d31047b4a7d733b7e01fb1d4144a
Jamie Strausbaugh <jstrausb86@gmail.com>
2019-04-24 16:39:04 -0400

    Added gatsby-source-wordpress plugin and specified configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,26 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `A sample site using gatsby-source-wordpress`,
+    subtitle: `Data fetched from a site hosted on wordpress.com`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: `gatsby-source-wordpress`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        baseUrl: `dev-gatbsyjswp.pantheonsite.io`,
+        protocol: `http`,
+        hostingWPCOM: false,
+        useACF: true,
       },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-glamor`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: `gatsby-plugin-typography`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        pathToConfigModule: `src/utils/typography.js`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
